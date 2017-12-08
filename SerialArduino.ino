0e883fb18f7135264528a06baccf0ce64da82a76
PotatoRice <randomkrud@gmail.com>
2017-12-08 11:38:43 -0800

    Add files via upload
    Code for my Arduino Uno I used when I did not have access to the force gauge for testing. Repeatedly sends values from an array over serial comms. Used to practice reading and processing serial data.

---
+++ SerialArduino.ino
@@ -0,0 +1,23 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+}
+
+int serialData;
+int data[] = {0, 0, 4, 3, 5, 6, 10, 15, 20, 12, 4, 1, 0, 0};
+int size = sizeof(data)/sizeof(int);
+void loop() {
+ for(int i = 0; i < size; i++){
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println(data[i]);
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  
+ }
+}
+
+
+
