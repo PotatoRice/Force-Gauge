ebbb42166aa73f51efb710585d665b9bd6d505e6
PotatoRice <randomkrud@gmail.com>
2018-01-04 12:47:44 -0800

    working versions
    finished some TODOs. Comments are psuedocode for when I rework the looping part. "stop" button on GUI does not work. Checkbutton does work.

---
+++ forcegauge.py
@@ -0,0 +1,110 @@
+from tkinter import Tk, Label, Button, Entry, Text, Checkbutton, IntVar
+import serial
+from time import gmtime, strftime
+import os
+from mark10 import serialLoop
+#from threading import Thread
+
+
+#TODO: button to stop/pause program
+#TODO: multithread so the above is possible, that way we aren't running an
+#uninterruptable 'while' loop -_-    or don't multithread and just rerun the
+#individual process each time (but only do the serial setup once)
+'''
+def serialSetup(args):
+    sets up connection through serial port and tells mark10 to transmit
+def serialPass(args):
+    reads/process data chunk
+runLoop = False
+button start (command: runLoop = True)
+button stop( command: runLoop = False, sysexit)
+button pause
+button resume
+
+stop and resume begin disabled, and are enabled when their counterparts are
+pressed, and vice versa
+
+while runLoop:
+    serialPass(args)
+    should work because serial read waits until the the required bytes are
+    read, so this loop won't try to run as fast as possible since it hangs
+    a bit. Might have buffer issues from going between iterations
+need to make sure the GUI loop is still being checked as the while loop runs
+that way the runLoop variable can be updated properly
+
+    '''
+#TODO: automatically close + reopen text file after certain amount of time
+#^done, tested once, needs more testing
+
+
+
+
+
+class ForceGaugeGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("FORCE GAUGE READER 3005")
+
+        self.t1 = Label(master, text="File Path:")
+        self.t1.pack()
+            
+        self.pathField = Entry(master, width=60)
+        self.pathField.pack()
+        #sets our default filepath as our current directory
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.pathField.insert(0, dir_path)
+
+        self.t2 = Label(master, text="Port: ")
+        self.t2.pack()
+        self.portField = Entry(master, width=10)
+        self.portField.pack()
+        self.portField.insert(0, "COM3")
+
+        self.t3 = Label(master, text="Dump data interval (minutes):")
+        self.t3.pack()
+        self.intervalField = Entry(master, width = 5)
+        self.intervalField.pack()
+        self.intervalField.insert(0, "60")
+
+        CheckVar = IntVar()
+        self.dataOption = Checkbutton(master, text= "Output only maximums",
+                                      variable = CheckVar)
+        self.dataOption.select()
+        self.dataOption.pack()
+        
+
+        #runs our loop when we press the button, with the parameters from the
+        #entry fields
+        def run():
+            #sets our fullPath as our current directory + the current date and
+            #time so that our text file is saved in the format "2017-12-15 0924"
+            self.fullPath = self.pathField.get() + "\\" + \
+            strftime(("%Y-%m-%d %H%M")+ ".txt")
+            '''args = (self.portField.get(), self.fullPath, 1000, 10, 250,
+                       maxes, self.intervalField.get())
+            myThread = Thread1(args)
+            myThread.setName('Serial Loop')
+            myThread.start()'''
+            serialLoop(self.portField.get(), self.fullPath, 1000, 2, 250,
+                       CheckVar.get(), self.intervalField.get())
+            
+        self.run_button = Button(master, text="Start",
+                                 command = lambda: run())
+        self.run_button.pack()
+
+
+        #master quit should raise a SystemExit, which also prompts the program
+        #to properly close before it shuts down, which saves our data
+        self.close_button = Button(master, text="Close",
+                                   command = lambda: root.destroy())
+        self.close_button.pack()
+
+    
+
+    
+
+    
+
+root = Tk()
+my_gui = ForceGaugeGUI(root)
+root.mainloop()
---
+++ mark10.py
@@ -0,0 +1,189 @@
+import time
+import serial
+import msvcrt
+import sys
+#from threading import Thread
+
+'''class Thread1(Thread):
+    def __init__(self, args):
+        Thread.__init__(self)
+        self.args = args
+    def run(self):
+        serialLoop(args)'''
+
+'''
+def serialSetup(port, filePath, transferSpeed):
+    ser = serial.Serial()
+    ser.port = port
+    ser.timeout = None
+    ser.baudrate = 112500
+
+    print(ser.name)
+    ser.open()
+    file = open(filePath, "a")
+
+    ser.write(b"AOUT" + str(transferSpeed).encode('UTF-8') + b"\r\n")
+    ser.write(b"N\r\n")
+    print("receiving data")
+    try:
+        ser.read(1)
+    except KeyboardInterrupt:
+        pass
+
+def serialPass(dataSize, lowCutoff, outputMaxes):
+    try:
+    #First, we take in dataSize
+    #amount of bytes, then convert those bytes into a string, then take the
+    #parts we want from that string (just the numbers) and make a dataPoint list
+    #out of it. From there we either output all the data points, or check each
+    #number to see if its a max, then output the maxima. 
+    dataPoints = [0]
+            maximaSub = [0]
+            localMaxima = []
+            print("-------")
+            input = ser.read(dataSize)
+            stringList = input.decode().split("N\r\n")
+            for i in range(0, len(stringList)):
+                try:
+                    dataPoints.append(float(stringList[i]))
+                except ValueError:
+                    pass
+
+            #run this only if we checked the box to print out only maximums
+            #we check each data point to see if its at least as large as
+            #the points before and after it. If it is, we record that subscript
+            #into the maximaSub list. Then we loop through the maximaSub list
+            #and add the dataPoint at that index to the localMaxima list, but
+            #only if its index is far enough away from the index before it. This
+            #is to prevent data like 12, 25, 20, 26, 4 to register as two spikes
+            #(25 and 26) when in reality we only want to treat that as 1 spike.
+            if outputMaxes == 1:   
+                dataPoints.append(0)
+                for i in range(1, len(dataPoints)-1):
+                    if (dataPoints[i] > lowCutoff):
+                        if ((dataPoints[i] >= dataPoints[i-1]) &
+                            (dataPoints[i] >= dataPoints[i+1])):
+                            maximaSub.append(i)
+                for j in range(1, len(maximaSub)):
+                    if maximaSub[j]-maximaSub[j-1] > 8:
+                        localMaxima.append(dataPoints[maximaSub[j]])
+    
+                for item in localMaxima:
+                    file.write(str(item) + "\n")
+                            
+                print("Data: " + str(dataPoints))
+                print("Maxima: " + str(localMaxima))
+            else:
+                print("Data: " + str(dataPoints))
+                for point in dataPoints:
+                    file.write(str(point) + "\n")
+                    
+            print(outputMaxes)
+            print(dumpInterval)
+            elapsed = time.clock() - start_time
+            if elapsed > float(dumpInterval*60):
+                file.close()
+                file = open(filePath, "a")
+            #if we've run over a certain number of minutes, dump the data we
+            #have so far and start recording again        
+'''
+
+def serialLoop(port, filePath, dataSize, lowCutoff, transferSpeed,
+               outputMaxes, dumpInterval):
+    #serial setup. Timeout is none so that we can use read(1) down below to wait
+    #for transmission to start
+    ser = serial.Serial()
+    ser.port = port
+    ser.timeout = None
+    ser.baudrate = 115200
+
+    print(ser.name)
+    ser.open()
+    file = open(filePath, "a")
+    
+
+    #Send commands to the M5i. 'N' tells it to send in Newtons,
+    #'AOUTn' tells it to automatically send data n times per second
+    ser.write(b"AOUT" + str(transferSpeed).encode('UTF-8') + b"\r\n")
+    ser.write(b"N\r\n")
+    
+ 
+    print("receiving data")
+    #wait until we receive data, program hangs here until data is received.
+    try:
+        ser.read(1)
+    except KeyboardInterrupt:
+        pass
+
+    start_time = time.clock()
+
+
+    try:
+    #following loop runs until interrupted. First, we take in dataSize
+    #amount of bytes, then convert those bytes into a string, then take the
+    #parts we want from that string (just the numbers) and make a dataPoint list
+    #out of it. From there we either output all the data points, or check each
+    #number to see if its a max, then output the maxima. 
+        while True:
+            dataPoints = [0]
+            maximaSub = [0]
+            localMaxima = []
+            print("-------")
+            input = ser.read(dataSize)
+            stringList = input.decode().split("N\r\n")
+            for i in range(0, len(stringList)):
+                try:
+                    dataPoints.append(float(stringList[i]))
+                except ValueError:
+                    pass
+
+            #run this only if we checked the box to print out only maximums
+            #we check each data point to see if its at least as large as
+            #the points before and after it. If it is, we record that subscript
+            #into the maximaSub list. Then we loop through the maximaSub list
+            #and add the dataPoint at that index to the localMaxima list, but
+            #only if its index is far enough away from the index before it. This
+            #is to prevent data like 12, 25, 20, 26, 4 to register as two spikes
+            #(25 and 26) when in reality we only want to treat that as 1 spike.
+            if outputMaxes == 1:   
+                dataPoints.append(0)
+                for i in range(1, len(dataPoints)-1):
+                    if (dataPoints[i] > lowCutoff):
+                        if ((dataPoints[i] >= dataPoints[i-1]) &
+                            (dataPoints[i] >= dataPoints[i+1])):
+                            maximaSub.append(i)
+                for j in range(1, len(maximaSub)):
+                    if maximaSub[j]-maximaSub[j-1] > 8:
+                        localMaxima.append(dataPoints[maximaSub[j]])
+    
+                for item in localMaxima:
+                    file.write(str(item) + "\n")
+                            
+                print("Data: " + str(dataPoints))
+                print("Maxima: " + str(localMaxima))
+            else:
+                print("Data: " + str(dataPoints))
+                for point in dataPoints:
+                    file.write(str(point) + "\n")
+                    
+            print(outputMaxes)
+            print(dumpInterval)
+            elapsed = time.clock() - start_time
+            if elapsed > float(dumpInterval*60):
+                file.close()
+                file = open(filePath, "a")
+            #if we've run over a certain number of minutes, dump the data we
+            #have so far and start recording again
+            
+    #if program ends incorrectly, COM port won't get closed (not a problem)
+    #and the file won't be
+    #written to (BIG problem). That's why we close both in case of a keyboard
+    #interrupt (ctrl + c) or a system exit
+    except (KeyboardInterrupt, SystemExit):
+        ser.close()
+        file.close()
+
+    ser.close()
+    file.close()
+
+
